1c4c7c201f981c5827a65827c90779dbd24a9c28
Holger Brunn <mail@hunki-enterprises.com>
2023-12-13 15:49:15 +0100

    [MIG] ps_timesheet_invoicing (preliminary commit)

--- ps_timesheet_invoicing/models/__init__.py
+++ ps_timesheet_invoicing/models/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from . import analytic
 from . import account_invoice
+from . import analytic
 from . import analytic_invoice
 from . import account_move
 from . import hr_timesheet_sheet
