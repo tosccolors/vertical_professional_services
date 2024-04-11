9a41a1fb2d7f30b34048be4a64419a41e6066547
Holger Brunn <mail@hunki-enterprises.com>
2024-04-11 17:54:51 +0200

    [ADD] squelch all errors when importing invoices via mail

--- ps_account/wizards/account_invoice_import.py
+++ ps_account/wizards/account_invoice_import.py
@@ -1,5 +1,10 @@
 # Copyright 2024 Hunki Enterprises BV
-from odoo import api, models
+import logging
+from base64 import b64decode
+
+from odoo import _, api, models
+
+logger = logging.getLogger(__name__)
 
 
 class AccountInvoiceImport(models.TransientModel):
@@ -11,3 +16,38 @@ class AccountInvoiceImport(models.TransientModel):
         if result["move_type"] == "in_invoice" and result.get("ref"):
             result["supplier_invoice_number"] = result.pop("ref")
         return result
+
+    @api.model
+    def create_invoice_webservice(
+        self,
+        invoice_file_b64,
+        invoice_filename,
+        origin,
+        company_id=None,
+        email_from=None,
+    ):
+        try:
+            super().create_invoice_webservice(
+                invoice_file_b64,
+                invoice_filename,
+                origin,
+                company_id=company_id,
+                email_from=email_from,
+            )
+        except Exception as e:
+            logger.error(
+                "Failed to import invoice from mail attachment %s. Error: %s",
+                invoice_filename,
+                e,
+            )
+            logger.info("Creating empty invoice anyways")
+            invoice = self.env["account.move"].create(
+                {
+                    "move_type": "in_invoice",
+                    "partner_id": self.env.user.partner_id.id,
+                }
+            )
+            invoice.message_post(
+                body=_("Error when importing mail from %s: %s") % (email_from, e),
+                attachments=[(invoice_filename, b64decode(invoice_file_b64))],
+            )
